186de3f1940399c8881bc04d2cc4ea64a8ed6b14
thaidt <thaidt@vng.com.vn>
2025-04-28 16:30:37 +0700

    Add custom number generation feature and update UI

--- app.js
+++ app.js
@@ -26,3 +26,45 @@ function generateNumbers() {
     }
     document.getElementById('result').innerText = pairs.join(', ');
 }
+
+function splitNumbers(numbers) {
+    const result = [];
+    numbers.forEach(num => {
+        if (num >= 100) {
+            const strNum = num.toString();
+            const firstPair = strNum.slice(0, 2);
+            const secondPair = strNum.slice(1);
+            result.push(firstPair, secondPair);
+        } else {
+            result.push(num.toString());
+        }
+    });
+    return result;
+}
+
+function randomSlip(lists) {
+    
+    var randomNumbers = [];
+    while (randomNumbers.length < 6) {
+        var randomIndex = Math.floor(Math.random() * lists.length);
+        var randomNumber = lists[randomIndex];
+        if (!randomNumbers.includes(randomNumber)) {
+            randomNumbers.push(randomNumber);
+        }
+    }
+    return randomNumbers;
+}
+
+function toggleCustomInput() {
+    const customInput = document.getElementById('custom_input');
+    const lotteryType = document.getElementById('lottery_type').value;
+}
+function generateCustomNumbers() {
+    const customInput = document.getElementById('custom_input').value;
+    const numbers = customInput.split(',').map(num => num.trim());
+    let pairs = [];
+    lists = splitNumbers(numbers);
+    pairs = randomSlip(lists);
+    document.getElementById('result').innerText = pairs.join(', ');
+}
+    
